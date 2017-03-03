1b607c82089e4141ca09891bdae3a86f51626665
Vohidjon <abduvohid.karimjonov@gmail.com>
2017-03-03 12:27:07 +0500

    implemented candidate service

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,14 +1,46 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Candidate} from './candidate.model';
+import {CandidateService} from 'candidate.service';
+
+import {Observable} from 'rxjs/Observable';
+import {Subject} from 'rxjs/Subject';
+
+// Observable class extensions
+import 'rxjs/add/observable/of';
+// Observable operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+
 @Component({
   moduleId: module.id,
   selector: 'app',
   templateUrl: 'app.html',
 })
-export class AppComponent {
-  candidates: Candidate[] = [];
-  query: string;
-  sayHello(){
-    alert(this.query);
+export class AppComponent implements OnInit{
+  candidates: Observable<Candidate[]>;
+  private queries = new Subject<string>();
+  constructor(private candidateService: CandidateService) {
+  }
+
+  search(query: string): void {
+    this.queries.next(query);
+  }
+
+  ngOnInit(): void {
+    this.candidates = this.queries
+      .debounceTime(300) // wait 300ms after each keystroke before considering the term
+      .distincUnitChanged() // ignore if next search query is the same as previous
+      .switchMap(query => // switch to new observable each time the term changes
+        // return the http search observable
+        query
+          ? this.candidateService.search(query)
+          // or the observable of empty candidates if there was no search term
+          : Observable.of<Candidate[]>([])
+          .catch(error => {
+            console.log(error);
+            return Observable.of<Candidate[]>([]);
+          })
+      );
   }
 }
---
+++ client/app/candidate.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import {Candidate} from 'candidate.model';
+
+@Injectable()
+export class CandidateService {
+  constructor(private http: Http) {
+  }
+
+  search(term: string): Observable<Candidate[]> {
+    return this.http
+      .get(`api/candidate?q=${term}`)
+      .map(response => response.json().data as Candidate[]);
+  }
+}
